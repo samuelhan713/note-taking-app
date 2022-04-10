cb4666e0c4ef49772e9ab49e4007074fa007c657
Samuel Han <60247381+samuelhan713@users.noreply.github.com>
2022-04-11 00:24:22 +0900

    Add files via upload

---
+++ src/components/Sidebar.js
@@ -0,0 +1,34 @@
+import './Main.css';
+import useWindowDimensions from '../windowResize';
+function Sidebar({notes, onAddNote, active, setActive, sidebarActive, handleSwitch}) { //instead of props.notes, you can just use notes
+    const { width, height } = useWindowDimensions();
+
+    return (
+        <div className={`sidebar ${sidebarActive ? "activeComponent" : "false"}`}>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+            <div className="sidebar-header">
+                <label htmlFor="modal"><img src="https://yt3.ggpht.com/IpUd8bbQ8AFVis-r0ypfvHvB1t8KTJWLOsbBxWbYNH24e_WIwYNwURqa009GWBy56zCrn52dqAc=s900-c-k-c0x00ffffff-no-rj"/></label>
+                <div>My Notes</div>
+                <span className="material-icons" id="note_add" onClick={onAddNote}>note_add</span>
+            </div>
+            <div className="search">
+                <span className="material-icons">search</span>
+                <span id="searchText">Search all notes</span>
+            </div>
+            <div className="sidebar-notes">
+                {notes.map((note) => ( 
+                    <div className={`sidebar-note ${note.id === active && "active"}`} onClick={() => {setActive(note.id); (width <= 500 && handleSwitch());}}>
+                        <div className="note-title">
+                            <h5>{note.text.length !== 0 ? note.text : "New Note"}</h5>
+                        </div>
+                        <div className="note-date">{note.date}</div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar;
+
+//pass the id for each note as the key when mapping over things
---
+++ src/components/TextArea.js
@@ -0,0 +1,101 @@
+import './Main.css';
+import React from "react";
+import { WithContext as ReactTags } from "react-tag-input";
+import useWindowDimensions from '../windowResize';
+import { useState } from 'react';
+
+
+
+function TextArea({handleNoteDelete, activeNote, onEdit, textAreaActive, handleSwitch}) {
+    const { height, width } = useWindowDimensions();
+    const d = new Date();
+
+    
+    const onType = (field, value) => {
+        onEdit(
+            {
+                ...activeNote,
+                [field]:value,
+                date: d.toISOString().slice(0,10).replace(/-/g,"/") + ", " + d.toISOString().slice(11,19).replace(/-/g,""),
+            }
+        )
+    }
+
+    const onNoteDelete = () => {
+        handleNoteDelete(activeNote);
+    }
+
+    const handleDelete = (i) => {
+        console.log("A tag was deleted!");
+        activeNote.noteTags.splice(i, 1);
+    }
+
+    const handleAddition = (tag) => {
+        activeNote.noteTags.push(tag);
+    }
+
+    const handleDrag = (tag, currPos, newPos) => {//????
+        /* const newTags = activeNote.noteTags.slice(); */
+        activeNote.noteTags.splice(currPos, 1);
+        activeNote.noteTags.splice(newPos, 0, tag);
+
+        activeNote.noteTags = [...activeNote.noteTags];
+        /* setTags(newTags); */
+    }
+
+    const handleTagClick = (index) => {
+        console.log("The tag at index " + index + "was clicked!");
+    }
+
+    const KeyCodes = {
+        enter: 13
+    };
+
+    const delimiters = [KeyCodes.enter];
+
+    if (!activeNote) {
+        return (
+            <div className={`text-main ${textAreaActive ? "activeComponent" : "false"}`}>
+                <div className="text-main-header">
+                    <span className="material-icons" onClick={handleSwitch}>arrow_back</span>
+                    <span className="material-icons">notification_add</span>
+                    <span className="material-icons">person_add_alt</span>
+                    <span className="material-icons" onClick={onNoteDelete}>delete</span>
+                </div> 
+            </div>
+        )
+    }
+    return (
+        <div className={`text-main ${textAreaActive ? "activeComponent" : "false"}`}>
+            <div className="text-main-header">
+                <span className="material-icons" onClick={handleSwitch}>arrow_back</span>
+                <span className="material-icons">notification_add</span>
+                <span className="material-icons">person_add_alt</span>
+                <span className="material-icons" onClick={onNoteDelete}>delete</span>
+            </div>
+            <div className="main-textarea">
+                <textarea value={activeNote.text} onChange={(e) => onType("text", e.target.value)}/>
+                <div>width: {width} ~ height: {height}</div>
+            </div>
+            <div className="text-main-footer">
+                <div className="tags">
+                    <ReactTags
+                        tags={activeNote.noteTags}
+                        delimiters={delimiters}
+                        handleDelete={handleDelete}
+                        handleAddition={handleAddition}
+                        handleDrag={handleDrag}
+                        handleTagClick={handleTagClick}
+                        inputFieldPosition="inline"
+                        placeholder="Enter a tag"
+                        autocomplete
+                        autofocus={false}
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TextArea;
+
